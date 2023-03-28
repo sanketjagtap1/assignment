46ec2dc512f8639975e04d6675814b64407cd172
Prankz10 <catch.pranit1@gmail.com>
2023-03-28 17:10:31 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
-
 const routes: Routes = [
   {path: '', component: HomePageComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'profile', component: ProfileComponent},
+  {path: 'about us', component: AboutUsComponent},
 ];
 
 @NgModule({
